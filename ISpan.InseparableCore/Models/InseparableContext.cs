e671aa53c07c7fa6d1ab0d2793650b86ff61470e
CarlHsu <ab7991373@gmail.com>
2023-04-06 15:45:35 +0800

    update

--- ISpan.InseparableCore/Models/InseparableContext.cs
+++ ISpan.InseparableCore/Models/InseparableContext.cs
@@ -57,8 +57,7 @@ namespace ISpan.InseparableCore.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=inseparable.database.windows.net;Initial Catalog=Inseparable;Persist Security Info=True;User ID=ISpan230117;Password=IN2023Separable_0117");
+
             }
         }
 
