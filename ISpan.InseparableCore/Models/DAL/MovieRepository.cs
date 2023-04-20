4fb0085ab244940c9e9d9e96d9635435b9c6bb61
Jia <vernalin1422@gmail.com>
2023-04-20 16:29:43 +0800

    兩層更新

---
+++ ISpan.InseparableCore/Models/DAL/MovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ISpan.InseparableCore.Models.DAL
+{
+    public class MovieRepository
+    {
+        private readonly InseparableContext _db;
+        public MovieRepository(InseparableContext db)
+        {
+            _db = db;
+        }
+    }
+}
