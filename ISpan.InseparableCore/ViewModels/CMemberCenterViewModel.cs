327efe3406bc9424907895e3ad37243c76bb8b0e
CarlHsu <ab7991373@gmail.com>
2023-04-21 09:18:50 +0800

    update, hide the unused tab

--- ISpan.InseparableCore/ViewModels/CMemberCenterViewModel.cs
+++ ISpan.InseparableCore/ViewModels/CMemberCenterViewModel.cs
@@ -58,7 +58,7 @@ namespace ISpan.InseparableCore.ViewModels
 
         [Display(Name = "目前密碼")]
         [DataType(DataType.Password)]
-        [StringLength(100, ErrorMessage = "新的密碼長度至少為 {2} 個字元。", MinimumLength = 4)]
+        [StringLength(100, ErrorMessage = "密碼長度至少為 {2} 個字元。", MinimumLength = 4)]
         [Required(ErrorMessage = "請輸入目前密碼")]
         public string? CurrentPassword
         {
