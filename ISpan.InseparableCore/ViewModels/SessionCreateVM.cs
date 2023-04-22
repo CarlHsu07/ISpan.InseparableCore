77f0c614b290e5696676b1bb69eafa19bd662066
Jia <vernalin1422@gmail.com>
2023-04-22 23:41:44 +0800

    todo select不會提醒必填

--- ISpan.InseparableCore/ViewModels/SessionCreateVM.cs
+++ ISpan.InseparableCore/ViewModels/SessionCreateVM.cs
@@ -1,6 +1,7 @@
 ﻿using ISpan.InseparableCore.Models;
 using ISpan.InseparableCore.Models.BLL.Cores;
 using ISpan.InseparableCore.Models.BLL.Dtos;
+using System.ComponentModel.DataAnnotations;
 
 namespace ISpan.InseparableCore.ViewModels
 {
@@ -13,11 +14,23 @@ namespace ISpan.InseparableCore.ViewModels
             _sessions = new SessionCreateDto();
         }
         public int FSessionId { get { return _sessions.FSessionId; } set { _sessions.FSessionId = value; } }
+        [Display(Name = "電影")]
+        [Required(ErrorMessage = "{0}必填")]
         public int FMovieId { get { return _sessions.FMovieId; } set { _sessions.FMovieId = value; } }
+        [Display(Name = "影廳")]
+        [Required(ErrorMessage = "{0}必填")]
         public int FRoomId { get { return _sessions.FRoomId; } set { _sessions.FRoomId = value; } }
+        [Display(Name = "影城")]
+        [Required(ErrorMessage = "{0}必填")]
         public int FCinemaId { get { return _sessions.FCinemaId; } set { _sessions.FCinemaId = value; } }
+        [Display(Name = "日期")]
+        [Required(ErrorMessage = "{0}必填")]
         public DateTime FSessionDate { get { return _sessions.FSessionDate; } set { _sessions.FSessionDate = value; } }
+        [Display(Name = "時間")]
+        [Required(ErrorMessage = "{0}必填")]
         public TimeSpan FSessionTime { get { return _sessions.FSessionTime; } set { _sessions.FSessionTime = value; } }
+        [Display(Name = "票價")]
+        [Required(ErrorMessage = "{0}必填")]
         public decimal FTicketPrice { get { return _sessions.FTicketPrice; } set { _sessions.FTicketPrice = value; } }
 
     }
