b41d7f6f559159f95008073596576ffe87ba11a5
CarlHsu <ab7991373@gmail.com>
2023-05-03 11:37:04 +0800

    update, fix GenerateMemberId

--- ISpan.InseparableCore/Models/BLL/MemberService.cs
+++ ISpan.InseparableCore/Models/BLL/MemberService.cs
@@ -17,40 +17,45 @@ namespace ISpan.InseparableCore.Models.BLL
         // 產生 會員ID 的方法
         public string GenerateMemberId()
         {
-
-            DateTime now = DateTime.Now; // 取得現在時間
+            String todayDate = DateTime.Now.ToString("yyyyMMdd"); // 今天日期
+            string newMemberID = string.Empty; // 新的會員ID
             int newSequence = 0; // 新的序號
 
-            // todo 會員ID，先抓出最後一筆的會員ID，拆分成日期與序號，檢查日期
+            string lastMemberID = _context.TMembers
+                .OrderByDescending(m => m.FSignUpTime)
+                .FirstOrDefault()?.FMemberId ?? null;
 
-            // 查詢當日已經新增的會員數量
-           // int todayNewMemberCount = _context.TMembers.Count(m => m.FSignUpTime.GetValueOrDefault().Date == now.Date);
-            int todayNewMemberCount = _context.TMembers.Count();
-            if (todayNewMemberCount == 0)
+            if (string.IsNullOrEmpty(lastMemberID)) // 若DB中沒任何會員
             {
-                // 新的序號為當日新會員數量加1
-                newSequence = todayNewMemberCount + 1;
+                newMemberID = CreateFirstMemberIDToday(todayDate);
             }
             else
             {
-                string lastMemberSequence = _context.TMembers
-                    .OrderByDescending(m => m.FSignUpTime)
-                    .FirstOrDefault().FMemberId;
+                string dateString = lastMemberID.Substring(1, 8);
 
-                //todo 檢查是不是今天 lastMemberSequence M 20230502 00002
-
-                int length = lastMemberSequence.Length;
-                string lastFiveChars = length >= 5 ? lastMemberSequence.Substring(length - 5) : lastMemberSequence;
-                newSequence = int.Parse(lastFiveChars) + 1;
+                if (dateString == todayDate) // 今天有會員註冊
+                {
+                    int length = lastMemberID.Length;
+                    string lastFiveChars = length >= 5 ? lastMemberID.Substring(length - 5) : lastMemberID;
+                    newSequence = int.Parse(lastFiveChars) + 1;
+                    newMemberID = "M" + todayDate + newSequence; // 將日期和序號結合，形成 newMemberID，例如 M2023050300001
+                }
+                else
+                {
+                    newMemberID = CreateFirstMemberIDToday(todayDate);
+                }
             }
 
-            // 將序號轉換為固定長度的字串，補足至 5 位數，補足的字元為 0
-            string sequenceString = newSequence.ToString().PadLeft(5, '0');
+            return newMemberID;
+        }
 
-            // 將日期和序號結合，形成 FMemberId，格式為 yyyyMMdd-序號
-            string fMemberId = "M" + now.ToString("yyyyMMdd") + sequenceString;
+        // 產生 今天第一個會員ID 的方法
+        private static string CreateFirstMemberIDToday(string todayDate)
+        {
+            string newSequence = "1"; // 第一位會員，序號為1
+            string sequenceString = newSequence.PadLeft(5, '0'); // 將序號轉為固定長度的字串，補'0'至5位數
 
-            return fMemberId;
+            return "M" + todayDate + sequenceString; // 將日期和序號結合，形成 newMemberID，例如 M2023050300001
         }
 
         //產生 會員註冊時間 的方法
