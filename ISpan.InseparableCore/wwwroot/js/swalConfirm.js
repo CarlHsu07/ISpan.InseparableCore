7ebb62e77bbce425f5cd0d2acff865dff3d320de
j1mmybean <carb4305@gmail.com>
2023-05-08 16:20:16 +0800

    go

--- ISpan.InseparableCore/wwwroot/js/swalConfirm.js
+++ ISpan.InseparableCore/wwwroot/js/swalConfirm.js
@@ -6,7 +6,13 @@
 		confirmButtonColor: '#3085d6',
 		cancelButtonColor: '#d33',
 		confirmButtonText: '確定',
-		cancelButtonText: '取消'
+		cancelButtonText: '取消',
+		customClass: {
+			title: 'my-custom-font',
+			content: 'my-custom-font',
+			confirmButton: 'my-custom-font',
+			cancelButton: 'my-custom-font'
+		}
 	}).then((result) => {
 		if (result.isConfirmed) {
 			function1();
