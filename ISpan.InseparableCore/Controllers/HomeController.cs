2f30f6ee541f6988f31c3aab0672b682f1857524
CarlHsu <ab7991373@gmail.com>
2023-05-09 15:59:02 +0800

    update

--- ISpan.InseparableCore/Controllers/HomeController.cs
+++ ISpan.InseparableCore/Controllers/HomeController.cs
@@ -67,6 +67,11 @@ namespace ISpan.InseparableCore.Controllers
                 ModelState.AddModelError(nameof(CMemberRegisterVM.Email), "Email錯誤，找不到您的帳號");
             }
 
+            if (member.FIsEmailVerified == false)
+            {
+                ModelState.AddModelError(nameof(CMemberRegisterVM.Email), "Email尚未驗證通過，請至信箱收驗證信並進行驗證");
+            }
+
             if (ModelState.IsValid) // 驗證通過
             {
                 if (member != null && CPasswordHelper.VerifyPassword(model.Password, member.FPasswordHash, member.FPasswordSalt))
